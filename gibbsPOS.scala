510a7baa1dfdcad15bffa3194868735e59bf2d84
mconnor <mconnor@202b6e8a-0321-0410-ab3f-db4cf95c4ef8>
2010-09-27 01:31:04 +0000

    Initial Compile, reads in column file, some counter start
    git-svn-id: svn://mc2net.gotdns.com/repos/gibbsPOS/trunk@680 202b6e8a-0321-0410-ab3f-db4cf95c4ef8

---
+++ gibbsPOS.scala
@@ -0,0 +1,85 @@
+import io.Source
+import collection.mutable.{HashMap, ArrayBuffer}
+import util.Random
+
+object gibbsPOS {
+    class Lexicon[A] {
+	val map = new HashMap[A,Int]()
+	private var nextID = 1
+	def apply(n:A):Int = 
+	    map.get(n) match {
+		case Some(i) => i
+		case None => {
+		    map.put(n,nextID)
+		    nextID = nextID+1
+		    nextID-1
+		}
+	    }
+	
+	def numID = nextID //Includes the 0th ID
+    }
+
+    class Counter(N:Int) {
+    	val c = {val t = new ArrayBuffer[Int]
+		 for (i <- 0 until N) t += 0
+		 t}
+	def apply(n:Int):Int = c(n)
+	def inc(a:Int) = c.update(a,c(a)+1)
+	def dec(a:Int) = c.update(a,c(a)-1)
+    }
+
+    class POSdata(file:String) {
+	val wordLex = new Lexicon[String]
+	val tagLex = new Lexicon[String]
+
+	//Given a line from a column file, return (word id, tag id) pair
+	//  if a blank line return (0,0) special marker
+	def POSColPair(line:String):(Int, Int) = {
+	    val cols = line.split("\\s+")
+	    if (cols.length < 2) (0,0)	
+	    else (tagLex(cols(0)), tagLex(cols(1)))
+	}
+
+	def load(source:Source):List[(Int, Int)] = 
+	    List((0,0)) ++ source.getLines.map(POSColPair).toList
+	
+	val data = load(Source.fromFile(file))
+	val nWords = wordLex.numID
+	val nLabels = tagLex.numID
+    }
+
+    class POSState (N: Int, pos: POSdata) {
+	val assign = new ArrayBuffer[Int]
+	
+	//state t->t' transition count
+	val tTrans = new ArrayBuffer[Counter]
+	for (i <- 0 until N) tTrans += new Counter(N)
+
+	//state t->word w transition count
+	val wEmit = new ArrayBuffer[Counter]
+	for (i <- 0 until N) tTrans += new Counter(pos.nWords)
+
+	//state t count
+	val tCount = new Counter(N)
+
+	//Initialize with uniformly random state assignments to all
+	// words
+	def initialize(r:Random) = {
+	    for (((w,t),i) <- pos.data.zipWithIndex) {
+		val s = if (w == 0) 0 else r.nextInt(N)
+		assign(i) += s
+		tCount.inc(s)
+		wEmit(s).inc(w)
+		if (i > 0) {
+		    tTrans(assign(i-1)).inc(s)
+		}
+	    }
+	}
+    }
+
+    def main(args: Array[String]): Unit = {
+	if (args.length < 1) throw new Error("Usage: gibbsPOS <N> <POS col>")
+	val posTxt = new POSdata(args(1))
+	println("Data: " + posTxt.data)
+    }
+}
